c81dc346f82b256561d9f1d3dc55645e5a019c2d
aaljo2 <aaljo2@naver.com>
2026-04-05 05:39:07 +0900

    오류 해결

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: [
-      { find: "@", replacement: "/src" }, // '@'를 'src' 폴더로 치환
-    ],
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  optimizeDeps: {
+    include: ["cytoscape", "react-cytoscapejs"],
   },
 });
